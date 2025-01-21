224ffc4caf0d15c760afb968994fec59fce72bde
AnandaMulia <126226998+DevNandaMulia@users.noreply.github.com>
2025-01-21 12:34:33 +0700

    Update pelanggan.php

--- pelanggan.php
+++ pelanggan.php
@@ -205,8 +205,6 @@ $h2 = mysqli_num_rows($h1); //jumlah pelanggan
                             <div class="text-muted">Website 2025</div>
                             <div>
                                 <a href='Data' target='_blank'>My Profile</a>
-                                &middot;
-                                <a href='paralax' target='_blank'>My About</a>
                             </div>
                         </div>
                     </div>
